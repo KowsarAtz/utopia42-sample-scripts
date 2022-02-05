7f26eb1308df5b3a0ac2a23e0af704feb558a9e5
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-05 09:15:08 +0330

    Add label to itemas

--- BigBlockBuilder.js
+++ BigBlockBuilder.js
@@ -1,9 +1,9 @@
 console.log("Running BigBlockBuilder");
 console.log(Inputs);
 
-var x = Inputs.X;
-var y = Inputs.Y;
-var z = Inputs.Z;
+var x = Inputs.x;
+var y = Inputs.y;
+var z = Inputs.z;
 
 for (let i = 0; i < Inputs.a; i++) {
   for (let j = 0; j < Inputs.b; j++) {
