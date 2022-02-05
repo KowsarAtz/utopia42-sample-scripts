b7e7f472c09fce8a7f081566c7a25b3616f90fc8
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-05 09:46:28 +0330

    Fix block type problem

--- BigBlockBuilder.js
+++ BigBlockBuilder.js
@@ -8,7 +8,7 @@ var z = Inputs.z;
 for (let i = 0; i < Inputs.a; i++) {
   for (let j = 0; j < Inputs.b; j++) {
     for (let k = 0; k < Inputs.c; k++) {
-      UtopiaApi.placeBlock(Inputs.type, x + i, y + j, z + k);
+      UtopiaApi.placeBlock(Inputs.blockType, x + i, y + j, z + k);
     }
   }
 }
