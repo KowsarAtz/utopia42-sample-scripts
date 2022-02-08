806f75eda40af0a901a863ead1bb7c2f29344ec5
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-08 10:35:09 +0330

    Fix BoringScript

--- BoringScript.js
+++ BoringScript.js
@@ -2,7 +2,11 @@ console.log("Running Boring Script");
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-for (let i = 0; i < Inputs.time; i++) {
-  console.log("Sleeping... " + i);
-  await sleep(1000);
+async function run(){
+    for (let i = 0; i < Inputs.time; i++) {
+        console.log("Sleeping... " + i);
+        await sleep(1000);
+    }
 }
+
+run();
