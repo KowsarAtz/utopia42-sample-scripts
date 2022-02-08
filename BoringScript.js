f1969072d04eb9b3e4a2438d9e26c0810c448152
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-08 10:24:53 +0330

    Enhance Boring script

--- BoringScript.js
+++ BoringScript.js
@@ -3,3 +3,8 @@ console.log("Running Boring Script");
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 sleep(Inputs.time);
+
+for (let i = 0; i < Inputs.time; i++) {
+  console.log("Sleeping... " + i);
+  sleep(1000);
+}
