11ff091b5f53b0738c9e0af6027ca123dd652125
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-08 11:41:36 +0330

    Add HeavyComputation script

---
+++ HeavyComputation.js
@@ -0,0 +1,10 @@
+console.log("Running HeavyComputation");
+
+async function main() {
+  for (let i = 0; i < Inputs.i; i++) {
+    for (let j = 0; j < Inputs.j; j++) {
+      let a = i + j;
+      let b = i * j;
+    }
+  }
+}
