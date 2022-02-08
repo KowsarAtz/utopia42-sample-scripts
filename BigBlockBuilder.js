62e3eed3db1f3a764d1d9c15fcd511bb31299970
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-08 11:34:23 +0330

    Add main method to scripts

--- BigBlockBuilder.js
+++ BigBlockBuilder.js
@@ -1,14 +1,14 @@
 console.log("Running BigBlockBuilder");
-console.log(Inputs);
 
-var x = Inputs.x;
-var y = Inputs.y;
-var z = Inputs.z;
-
-for (let i = 0; i < Inputs.a; i++) {
-  for (let j = 0; j < Inputs.b; j++) {
-    for (let k = 0; k < Inputs.c; k++) {
-      UtopiaApi.placeBlock(Inputs.blockType, x + i, y + j, z + k);
+async function main() {
+  var x = Inputs.x;
+  var y = Inputs.y;
+  var z = Inputs.z;
+  for (let i = 0; i < Inputs.a; i++) {
+    for (let j = 0; j < Inputs.b; j++) {
+      for (let k = 0; k < Inputs.c; k++) {
+        UtopiaApi.placeBlock(Inputs.blockType, x + i, y + j, z + k);
+      }
     }
   }
 }
--- BoringScript.js
+++ BoringScript.js
@@ -2,11 +2,9 @@ console.log("Running Boring Script");
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-async function run(){
+async function main(){
     for (let i = 0; i < Inputs.time; i++) {
         console.log("Sleeping... " + i);
         await sleep(1000);
     }
 }
-
-run();
