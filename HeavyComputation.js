622da939d796b5b6f631213b70327458d95e223f
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-09 11:14:34 +0330

    Add logging to HeavyComputation script

--- HeavyComputation.js
+++ HeavyComputation.js
@@ -2,6 +2,7 @@ console.log("Running HeavyComputation");
 
 async function main() {
   console.log("Running HeavyComputation -> main");
+  console.log(Inputs);
   for (let i = 0; i < Inputs.i; i++) {
     for (let j = 0; j < Inputs.j; j++) {
       let a = i + j;
