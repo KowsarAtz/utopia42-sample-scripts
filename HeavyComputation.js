14d2f26ded2fd16723a7fb8b5f6c187f842e3937
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-09 11:13:48 +0330

    Update HeavyComputationScript

--- HeavyComputation.js
+++ HeavyComputation.js
@@ -1,6 +1,7 @@
 console.log("Running HeavyComputation");
 
 async function main() {
+  console.log("Running HeavyComputation -> main");
   for (let i = 0; i < Inputs.i; i++) {
     for (let j = 0; j < Inputs.j; j++) {
       let a = i + j;
