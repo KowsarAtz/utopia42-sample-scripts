cf8d75f4344500cc0021261b8436cc2f59427a47
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-10 10:57:10 +0330

    Add logging to cage builder

--- CageBuilder.js
+++ CageBuilder.js
@@ -2,15 +2,17 @@ async function main() {
   console.log("Running CageBuilder");
 
   let pos = Inputs.playerPosition;
-  console.log("Player position: " + pos);
 
   let x = Math.round(pos.x);
   let y = Math.round(pos.y);
   let z = Math.round(pos.z);
 
+  console.log("Player position rounded: " + x + ", " + y + ", " + z);
+
   for (let zz = z - 1; zz <= z + 1; zz + 2) {
     for (let yy = y - 1; y <= y + 1; yy + 2) {
       for (let xx = x; x <= x + Inputs.height; xx++) {
+        console.log("Placing block at: " + xx + ", " + yy + ", " + zz);
         UtopiaApi.placeBlock(Inputs.blockType, xx, yy, zz);
       }
     }
