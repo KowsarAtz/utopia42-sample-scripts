638baf069b54da0b97478c75c85fa745f7531664
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-10 10:37:15 +0330

    Add CageBuilder

---
+++ CageBuilder.js
@@ -0,0 +1,12 @@
+console.log("Running CageBuilder");
+
+let pos = Inputs.playerPosition;
+log("Player position: " + pos);
+
+let x = Math.round(pos.x);
+let y = Math.round(pos.y);
+let z = Math.round(pos.z);
+
+// for (let zz = z; zz < z + 20; zz++) {
+
+// }
