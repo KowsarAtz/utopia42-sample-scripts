94c09a3092702110ae41d91ee47e5f074bc53b84
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-10 10:47:06 +0330

    Implement cage builder

--- CageBuilder.js
+++ CageBuilder.js
@@ -7,6 +7,10 @@ let x = Math.round(pos.x);
 let y = Math.round(pos.y);
 let z = Math.round(pos.z);
 
-// for (let zz = z; zz < z + 20; zz++) {
-
-// }
+for (let zz = z - 1; zz <= z + 1; zz + 2) {
+  for (let yy = y - 1; y <= y + 1; yy + 2) {
+    for (let xx = x; x <= x + Inputs.height; xx++) {
+      UtopiaApi.placeBlock(Inputs.blockType, xx, yy, zz);
+    }
+  }
+}
