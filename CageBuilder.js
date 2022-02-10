0461a31546b79a70cd80ba19eb0d8928b8703237
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-10 11:20:33 +0330

    Fix cage builder

--- CageBuilder.js
+++ CageBuilder.js
@@ -10,7 +10,7 @@ async function main() {
   console.log("Player position rounded: " + x + ", " + y + ", " + z);
 
   for (let zz = z - 1; zz <= z + 1; zz = zz + 2) {
-    for (let xx = x - 1; x <= x + 1; xx = xx + 2) {
+    for (let xx = x - 1; xx <= x + 1; xx = xx + 2) {
       for (let yy = x; yy <= x + Inputs.height; yy++) {
         console.log("Placing block at: " + yy + ", " + xx + ", " + zz);
         UtopiaApi.placeBlock(Inputs.blockType, yy, xx, zz);
