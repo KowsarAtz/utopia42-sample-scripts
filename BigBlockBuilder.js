c27397df33cf303802e319903bd87281d7c7e46c
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-12 09:52:50 +0330

    Update BBB

--- BigBlockBuilder.js
+++ BigBlockBuilder.js
@@ -7,7 +7,12 @@ async function main() {
   for (let i = 0; i < Inputs.a; i++) {
     for (let j = 0; j < Inputs.b; j++) {
       for (let k = 0; k < Inputs.c; k++) {
-        UtopiaApi.placeBlock(Inputs.blockType, x + i, y + j, z + k);
+        UtopiaApi.placeBlock(Inputs.blockType, x + i, y + j, z + k).handle(
+          function (res) {
+            console.log("placeResult");
+            console.log(res);
+          }
+        );
       }
     }
   }
