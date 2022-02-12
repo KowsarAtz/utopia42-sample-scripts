33d4e24d3048e4b1e888795ba6487dacda110958
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-12 11:06:30 +0330

    Update placeBlock

--- BigBlockBuilder.js
+++ BigBlockBuilder.js
@@ -7,12 +7,12 @@ async function main() {
   for (let i = 0; i < Inputs.a; i++) {
     for (let j = 0; j < Inputs.b; j++) {
       for (let k = 0; k < Inputs.c; k++) {
-        UtopiaApi.placeBlock(Inputs.blockType, x + i, y + j, z + k).then(
-          function (res) {
-            console.log("placeResult");
-            console.log(res);
-          }
-        );
+        try {
+          let res = await UtopiaApi.placeBlock(Inputs.blockType, x + i, y + j, z + k);
+          console.log(res);
+        } catch(e) {
+            console.log(e);
+        }
       }
     }
   }
