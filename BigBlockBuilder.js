061fd37687d7bc37ba05b50db9d658c57d9cf521
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-12 10:10:47 +0330

    Fix BBB

--- BigBlockBuilder.js
+++ BigBlockBuilder.js
@@ -7,7 +7,7 @@ async function main() {
   for (let i = 0; i < Inputs.a; i++) {
     for (let j = 0; j < Inputs.b; j++) {
       for (let k = 0; k < Inputs.c; k++) {
-        UtopiaApi.placeBlock(Inputs.blockType, x + i, y + j, z + k).handle(
+        UtopiaApi.placeBlock(Inputs.blockType, x + i, y + j, z + k).then(
           function (res) {
             console.log("placeResult");
             console.log(res);
