5af0b23321a163518f70a3ec65f811d8febc7ec7
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-02-20 07:32:11 +0330

    Modify sample script for scriptjs import test

---
+++ parser-samples/scriptjs-test/data copy 2.js
@@ -0,0 +1,7 @@
+module.exports.exportedFunction = () => {
+    console.log("exported sample function executed");
+}
+
+unExportedFunction = () => {
+    console.log("un-exported sample function executed");
+}
