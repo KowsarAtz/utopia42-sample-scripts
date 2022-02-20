9e17803949f527ae4b1936f09f8a9ff84ae340db
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-02-20 13:46:28 +0330

    Modify sample script for scriptjs import test

--- parser-samples/scriptjs-test/data copy 3.js
+++ parser-samples/scriptjs-test/data copy 3.js
@@ -1,6 +1,3 @@
 self.unExportedFunction = () => {
     console.log("un-exported sample function executed");
 }
-
-
-self.module
---
+++ parser-samples/scriptjs-test/test-scriptjs copy 6.js
@@ -0,0 +1,4 @@
+async function main(){
+    importScripts("https://cdn.jsdelivr.net/gh/KowsarAtz/utopia42-sample-scripts@main/parser-samples/scriptjs-test/main%20copy.js");
+    console.log(self)
+}
