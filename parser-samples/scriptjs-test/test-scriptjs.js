cf73828f3a5faffa581e07e8ae181742a240f7e6
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-02-20 06:58:08 +0330

    Modify sample script for scriptjs import test

--- parser-samples/scriptjs-test/test-scriptjs.js
+++ parser-samples/scriptjs-test/test-scriptjs.js
@@ -1,4 +1,2 @@
 importScripts("https://raw.githubusercontent.com/KowsarAtz/utopia42-sample-scripts/main/parser-samples/scriptjs-test/data.js");
-
-console.log("script loaded");
-console.log("sample", sample);
+sample();
