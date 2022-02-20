f5ced2e177867ac7653c7586fd050810a3df0c9c
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-02-20 13:50:02 +0330

    Modify sample script for scriptjs import test

---
+++ parser-samples/scriptjs-test/test-scriptjs copy 7.js
@@ -0,0 +1,4 @@
+async function main(){
+    importScripts("https://cdn.jsdelivr.net/gh/KowsarAtz/utopia42-sample-scripts@main/parser-samples/scriptjs-test/parse-magica-voxel.js");
+    console.log(self.parseMagicaVoxel)
+}
