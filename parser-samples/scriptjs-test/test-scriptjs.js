d28791334bc1dd53acb53cc969889bf6af2e9c9a
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-02-20 10:40:53 +0330

    Modify sample script for scriptjs import test

--- parser-samples/scriptjs-test/test-scriptjs.js
+++ parser-samples/scriptjs-test/test-scriptjs.js
@@ -1,17 +1,13 @@
 async function main(){
-    try{
-        importScripts("https://cdn.jsdelivr.net/gh/KowsarAtz/utopia42-sample-scripts@main/parser-samples/scriptjs-test/parse-magica-voxel.js")
-            .then(() => {
-                console.log("exports", self.module.exports)
-            })
-            .catch((e) => console.error("could not load lib:", e));
+    importScripts("https://cdn.jsdelivr.net/gh/KowsarAtz/utopia42-sample-scripts@main/parser-samples/scriptjs-test/parse-magica-voxel.js")
+        .then(() => {
+            console.log("exports", self.module.exports)
+        })
+        .catch((e) => console.error("could not load lib:", e));
 
-        importScripts("https://cdn.jsdelivr.net/gh/KowsarAtz/utopia42-sample-scripts@main/parser-samples/scriptjs-test/data%20copy%203.js")
-            .then(() => {
-                self.unExportedFunction()
-            })
-            .catch((e) => console.error("could not load sample:", e));
-    }
-    catch(e){
-    }
+    importScripts("https://cdn.jsdelivr.net/gh/KowsarAtz/utopia42-sample-scripts@main/parser-samples/scriptjs-test/data%20copy%203.js")
+        .then(() => {
+            self.unExportedFunction()
+        })
+        .catch((e) => console.error("could not load sample:", e));
 }
