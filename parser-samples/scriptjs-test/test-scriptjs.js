51881086de0dfd92f30944ba2f4b03e586fa966d
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-02-20 10:42:24 +0330

    Modify sample script for scriptjs import test

--- parser-samples/scriptjs-test/test-scriptjs.js
+++ parser-samples/scriptjs-test/test-scriptjs.js
@@ -1,9 +1,9 @@
 async function main(){
-    importScripts("https://cdn.jsdelivr.net/gh/KowsarAtz/utopia42-sample-scripts@main/parser-samples/scriptjs-test/parse-magica-voxel.js")
-        .then(() => {
-            console.log("exports", self.module.exports)
-        })
-        .catch((e) => console.error("could not load lib:", e));
+    // importScripts("https://cdn.jsdelivr.net/gh/KowsarAtz/utopia42-sample-scripts@main/parser-samples/scriptjs-test/parse-magica-voxel.js")
+    //     .then(() => {
+    //         console.log("exports", self.module.exports)
+    //     })
+    //     .catch((e) => console.error("could not load lib:", e));
 
     importScripts("https://cdn.jsdelivr.net/gh/KowsarAtz/utopia42-sample-scripts@main/parser-samples/scriptjs-test/data%20copy%203.js")
         .then(() => {
