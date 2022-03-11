f8ffb61abf725ca102a3ece784a1cc9efcb6e42b
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-03-11 10:19:48 +0330

    Add castle vox

--- vox-importer-colored.js
+++ vox-importer-colored.js
@@ -73,7 +73,9 @@ async function main() {
         // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         // body: JSON.stringify(data) // body data type must match "Content-Type" header
     });
+
     console.log(response)
+    console.log(response.body)
     
     return;
 
