8931fdd29b51001fb72ac382d2676a9058ed90db
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-03-11 10:05:43 +0330

    Add castle vox

--- vox-importer-colored.js
+++ vox-importer-colored.js
@@ -57,13 +57,31 @@ var request = require("request");
 async function main() {
     const inputs = await UtopiaApi.getInputsFromUser(baseParams);
     importScripts(inputs.parserUrl);
+
+
+    const response = await fetch(inputs.voxUrl, {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: 'same-origin', // include, *same-origin, omit
+        // headers: {
+        //   'Content-Type': 'application/json'
+        //   // 'Content-Type': 'application/x-www-form-urlencoded',
+        // },
+        // redirect: 'follow', // manual, *follow, error
+        // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        // body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    console.log(response)
+    
+    return;
+
     request(inputs.voxUrl, (error, result, body) => {
         console.log(error);
         console.log(result);
         console.log(body);
     })
 
-    return;
     const buffer = await (
         await fetch(new Request(inputs.voxUrl))
     ).arrayBuffer();
